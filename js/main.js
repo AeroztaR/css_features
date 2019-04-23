48f73d0d21b61519cfe6047e63728cae826bb912
AeroztaR <jorguinhos89@gmail.com>
2019-04-24 04:09:20 +0500

    add burger menu nav and animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const menuBtn = document.querySelector('.menu-btn');
+const   menuBtn = document.querySelector('.menu-btn'),
+        menuNav = document.querySelector('.menu__nav');
 
 menuBtn.addEventListener('click', (e) => {
     e.preventDefault();
     menuBtn.classList.toggle('menu-btn--active');
+    menuNav.classList.toggle('menu__nav--active');
 });
 
